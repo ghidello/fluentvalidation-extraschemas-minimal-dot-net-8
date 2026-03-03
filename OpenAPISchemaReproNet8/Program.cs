b127072664af4c24ffba5e12d3fe35a1d13d2a1a
Alessandro Ghidini <ghidello@users.noreply.github.com>
2026-03-03 12:32:23 +0100

    confirm that the validation is actually working

--- OpenAPISchemaReproNet8/Program.cs
+++ OpenAPISchemaReproNet8/Program.cs
@@ -1,5 +1,7 @@
 using FluentValidation;
 using MicroElements.Swashbuckle.FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -29,18 +31,27 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", ([AsParameters] WeatherForecastRequest request) =>
-    {
-        var forecast = Enumerable.Range(1, request.PageSize).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
+app.MapGet("/weatherforecast",
+        Results<Ok<WeatherForecast[]>, ValidationProblem> ([AsParameters] WeatherForecastRequest request,
+            [FromServices] IValidator<WeatherForecastRequest> validator) =>
+        {
+            var validationResult = validator.Validate(request);
+            if (!validationResult.IsValid)
+            {
+                return TypedResults.ValidationProblem(validationResult.ToDictionary());
+            }
+
+            var forecast = Enumerable.Range(1, request.PageSize).Select(index =>
+                    new WeatherForecast
+                    (
+                        DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                        Random.Shared.Next(-20, 55),
+                        summaries[Random.Shared.Next(summaries.Length)]
+                    ))
+                .ToArray();
+
+            return TypedResults.Ok(forecast);
+        })
     .WithName("GetWeatherForecast");
 
 app.Run();
